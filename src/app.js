26c2aaa6d7c27edd79cabfa89c05d16c26d9c2e9
Velkez <velkezdev@gmail.com>
2025-04-26 19:46:54 -0500

    añadiendo inicialización de firebase admin

--- src/app.js
+++ src/app.js
@@ -4,6 +4,17 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const nodemailer = require("nodemailer"); // Importar nodemailer
 
+// Inicializar Firebase Admin
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
+  });
+}
+
 const app = express();
 
 // Middleware CORS
